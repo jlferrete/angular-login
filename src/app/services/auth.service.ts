37758ecfb50882fc94471f18e1ac7d128c7772d7
José Luis Ferrete <joseluisferrete@gmail.com>
2020-01-10 11:31:56 +0100

    Added Firebase and Rest services

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { UsuarioModel } from '../models/usuario.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private url = 'https://identitytoolkit.googleapis.com/v1/accounts:';
+  private apikey = 'AIzaSyBnJrxpRtyswIME16KnHCvcnXoKtT4s3nk';
+  
+  
+  // Crear nuevo usuario
+  // https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=[API_KEY]
+  
+
+  // Login
+  // https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=[API_KEY]
+  
+  constructor( private http: HttpClient) { }
+
+  logout() {
+
+  }
+
+  login( usuario: UsuarioModel ){
+
+  }
+
+  nuevoUsuario( usuario: UsuarioModel ) {
+    
+  } 
+
+}
+
+
